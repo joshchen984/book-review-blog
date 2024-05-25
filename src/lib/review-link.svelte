16fc13ece2399237de9c79e5169ed8d926adaae4
joshchen984 <joshchen984@gmail.com>
2024-05-25 11:10:33 -0700

    Make review link not take full page width

--- src/lib/review-link.svelte
+++ src/lib/review-link.svelte
@@ -5,9 +5,11 @@
 	export let dateCreated: Date;
 </script>
 
-<a href={`posts/${path}`} class="group mb-6 block">
+<a href={`posts/${path}`} class="group mb-6 block max-w-max">
 	<p>
-		<span class="font-medium text-primary-main group-hover:text-primary-light">{bookTitle}</span>
+		<span class="font-medium text-primary-main group-hover:text-primary-light group-hover:underline"
+			>{bookTitle}</span
+		>
 		<span class="font-medium">by {author}</span>
 	</p>
 	<span class="text-gray">{dateCreated.toDateString()}</span>
