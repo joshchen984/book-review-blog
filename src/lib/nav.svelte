c265448a25d8615c6119b9f126f298f2960dbd0a
joshchen984 <joshchen984@gmail.com>
2025-05-17 00:07:47 -0400

    Make newsletter button text meaning clearer

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -15,7 +15,7 @@
 	class="fixed top-0 z-30 flex h-20 w-screen flex-col items-center justify-around bg-nav shadow-sm shadow-nav sm:h-14 sm:flex-row sm:justify-end sm:px-8"
 >
 	<a href="/" class="sm:mr-5">Home</a>
-	<button on:click={() => modal.open()} class="sm:mr-5">Newsletter</button>
+	<button on:click={() => modal.open()} class="sm:mr-5">Subscribe to the Newsletter</button>
 	<form on:submit|preventDefault={handleSearch} class="w-5/6 sm:max-w-96">
 		<input class="w-full rounded-2xl px-4 py-0.5" placeholder="Search" bind:value={query} />
 	</form>
